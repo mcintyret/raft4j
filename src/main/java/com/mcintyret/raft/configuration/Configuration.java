10b017200b8c716adfde538a38c0419b862635ad
Tom McIntyre <tom.mcintyre5@gmail.com>
2016-01-24 20:40:59 +0000

    Configuration: add majoritySize

--- src/main/java/com/mcintyret/raft/configuration/Configuration.java
+++ src/main/java/com/mcintyret/raft/configuration/Configuration.java
@@ -8,11 +8,18 @@ public class Configuration {
 
     private final List<Peer> peers;
 
+    private final int majoritySize;
+
     public Configuration(List<Peer> peers) {
         this.peers = peers;
+        majoritySize = 1 + (peers.size() / 2);
     }
 
     public List<Peer> getPeers() {
         return peers;
+    }
+
+    public int getMajoritySize() {
+        return majoritySize;
     }
 }
